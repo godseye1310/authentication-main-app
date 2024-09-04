10089cd45bcd00715236ed327758a8f4bdc355a8
Avinash <avinashkmg1310@gmail.com>
2024-09-04 05:42:33 +0530

    +Auth form

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -3,40 +3,70 @@ import { useState, useRef } from 'react';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
+	const [isLogin, setIsLogin] = useState(true);
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+	const switchAuthModeHandler = () => {
+		setIsLogin((prevState) => !prevState);
+	};
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input
-            type='password'
-            id='password'
-            required
-          />
-        </div>
-        <div className={classes.actions}>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+	const submitHandler = (e) => {
+		e.preventDefult();
+
+		const enterdEmail = emailInputRef.current.value;
+		const enteredPassword = passwordInputRef.current.value;
+
+		if (isLogin) {
+		} else {
+			fetch(
+				'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyDzwEjIvWQsoay8pviwSR53woljwKRkOVY',
+				{
+					method: 'POST',
+					body: JSON.stringify({
+						email: enterdEmail,
+						password: enteredPassword,
+						returnSecureToken: true,
+					}),
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			).then((res) => {
+				if (res.ok) {
+				} else {
+					return res.json().then((data) => {
+						console.log(data);
+					});
+				}
+			});
+		}
+	};
+
+	return (
+		<section className={classes.auth}>
+			<h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+			<form onSubmit={submitHandler}>
+				<div className={classes.control}>
+					<label htmlFor="email">Your Email</label>
+					<input type="email" id="email" required ref={emailInputRef} />
+				</div>
+				<div className={classes.control}>
+					<label htmlFor="password">Your Password</label>
+					<input type="password" id="password" required ref={passwordInputRef} />
+				</div>
+				<div className={classes.actions}>
+					<button
+						type="button"
+						className={classes.toggle}
+						onClick={switchAuthModeHandler}
+					>
+						{isLogin ? 'Create new account' : 'Login with existing account'}
+					</button>
+				</div>
+			</form>
+		</section>
+	);
 };
 
 export default AuthForm;
